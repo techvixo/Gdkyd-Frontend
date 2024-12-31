b8453c60f4fc680bece4cfaa03c8c9a2e81507fa
sultanmahmud07 <asadkhan01862@gmail.com>
2024-12-31 23:44:06 +0600

    New domain add

--- Constant.js
+++ Constant.js
@@ -1,5 +1,5 @@
 
-// Wintec Live server =======================================
-// export const BASEURL = "http://145.223.21.187:8001/v1";
 // KYD Live server =======================================
-export const BASEURL ="http://147.79.117.103:8002/v1"         
+export const BASEURL = "https://api.gdkyd.com/v1";
+// KYD Live server =======================================
+// export const BASEURL ="http://147.79.117.103:8002/v1"         
