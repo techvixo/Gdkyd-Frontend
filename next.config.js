1c41850e2ce1462757e33a28704538660b4c143c
Fahad Hossain <105900280+fahadhossain24@users.noreply.github.com>
2025-01-02 00:41:56 +0600

    Update next.config.js |  serverless or static export

--- next.config.js
+++ next.config.js
@@ -4,9 +4,13 @@ const withNextIntl = createNextIntlPlugin();
  
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: ["145.223.21.187", "147.79.117.103", "gdkyd.com", "api.gdkyd.com"], 
-      },
+  images: {
+    domains: ["145.223.21.187", "147.79.117.103", "gdkyd.com", "api.gdkyd.com"],
+  },
+  // Add exportTrailingSlash for static export support
+  trailingSlash: true,  // Optional: This will generate clean URLs, like /about/ instead of /about
+  // Add target: 'serverless' for static export
+  target: 'serverless',
 };
  
 module.exports = withNextIntl(nextConfig);
