b1be1a75e59fd2dee50dadb43375d867251e7744
sultanmahmud07 <asadkhan01862@gmail.com>
2025-01-22 00:46:39 +0600

    Added google tag manager

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,28 +1,55 @@
 import { getLocale, getMessages } from "next-intl/server";
-import { NextIntlClientProvider } from 'next-intl';
+import { NextIntlClientProvider } from "next-intl";
 import "./globals.css";
-import Navbar from "../components/Shared/Navbar/Navbar";
 import NavbarWrap from "../components/Shared/Navbar/NavberWrap";
 import NewFooter from "../components/Shared/Footer/NewFooter";
 import ChatBox from "../components/Shared/ChatBox/ChatBox";
 import SocialIcons from "../components/Shared/SocialIcons/SocialIcons";
 import { Toaster } from "react-hot-toast";
+import Script from "next/script";
 
 export const metadata = {
   title: "Guangdong KYD Precision Machinery Co., Ltd.",
-  description: "Expert Manufacturer of Non-Woven Processing Equipment | Guangdong KYD Precision Machinery Co., Ltd.",
+  description:
+    "Expert Manufacturer of Non-Woven Processing Equipment | Guangdong KYD Precision Machinery Co., Ltd.",
 };
 
 export default async function RootLayout({ children }) {
   const locale = await getLocale();
-
-  // Providing all messages to the client
-  // side is the easiest way to get started
   const messages = await getMessages();
+
   return (
     <html lang={locale}>
+      <head>
+        <meta
+          name="google-site-verification"
+          content="h3e62bYsWEGHaX-2QCqFFXfOGtkjb5lMJ_FJpg0a6R0"
+        />
+        {/* Add Google Analytics Script */}
+        <Script
+          strategy="afterInteractive"
+          src={`https://www.googletagmanager.com/gtag/js?id=G-9GV6PTWLPM`}
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-9GV6PTWLPM');
+          `}
+        </Script>
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-W3BPJSZR');
+`}
+        </Script>
+      </head>
       <body className={``}>
-         <Toaster position="top-center" />
+        <Toaster position="top-center" />
         <NextIntlClientProvider messages={messages}>
           <NavbarWrap></NavbarWrap>
           <SocialIcons></SocialIcons>
@@ -30,6 +57,14 @@ export default async function RootLayout({ children }) {
           {children}
           <NewFooter></NewFooter>
         </NextIntlClientProvider>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-W3BPJSZR"
+            height="0"
+            width="0"
+            style="display:none;visibility:hidden"
+          ></iframe>
+        </noscript>
       </body>
     </html>
   );
